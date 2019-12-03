d761dcdde7f1ea90069f1a9cf7a992e25939432b
IvanNenov <nenow@abv.bg>
2019-12-04 00:47:57 +0200

    implemented auth helper

---
+++ NenovCars.client-app/src/helpers/Auth/Auth.ts
@@ -0,0 +1,15 @@
+export default class Auth {
+    /**
+     *  Checks if user is authenticated and returns true or false.
+     */
+    public static isUserAuthenticated(): boolean {
+        return localStorage.getItem('token') ? true : false;
+    }
+
+    /**
+     * Returns user's token if any else returns null.
+     */
+    public static getAuthToken(): string | null {
+        return localStorage.getItem('token');
+    }
+}
