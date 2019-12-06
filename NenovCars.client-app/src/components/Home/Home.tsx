7f1a131995faf513d3e04adb10d0499542f81dc5
IvanNenov <nenow@abv.bg>
2019-12-06 21:54:15 +0200

    Listing of all cars on home

--- NenovCars.client-app/src/components/Home/Home.tsx
+++ NenovCars.client-app/src/components/Home/Home.tsx
@@ -1,20 +1,44 @@
 import * as React from 'react';
 import AdsContainer from '../AdsContainer/AdsContainer';
+import { inject, observer } from 'mobx-react';
+import { IAdStore } from '../../stores/AdStore/AdStore';
+import LoadingIndicatior from '../LoadingIndicator/LoadingIndicator';
 
 interface HomeProps {
-
+    adStore?: IAdStore;
 }
 
 interface HomeState {
-
+    page: number;
+    isAdsLoading: boolean;
 }
 
+@inject('adStore')
+@observer
 export default class Home extends React.Component<HomeProps, HomeState> {
+    public state: HomeState = {
+        page: 1,
+        isAdsLoading: true
+    }
+
+    public async componentDidMount(): Promise<void> {
+        await this.props.adStore.getAllCars(this.state.page.toString());
+
+        if (this.props.adStore.carsContainer && this.props.adStore.carsContainer.allCars) {
+            this.setState({
+                isAdsLoading: false
+            })
+        }
+    }
+
     public render() {
         return (
-            <div>
-                <AdsContainer />
-            </div>
+            <>
+                {this.state.isAdsLoading ?
+                    <LoadingIndicatior /> :
+                    <AdsContainer carsList={this.props.adStore.carsContainer.allCars}/>}
+
+            </>
         );
     }
 }
