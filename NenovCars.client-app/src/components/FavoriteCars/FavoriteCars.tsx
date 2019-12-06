dc7483899cc2028dd4ebab38057b35f2d3986efe
IvanNenov <nenow@abv.bg>
2019-12-06 21:53:50 +0200

    Added FavoriteCars component

--- NenovCars.client-app/src/components/FavoriteCars/FavoriteCars.tsx
+++ NenovCars.client-app/src/components/FavoriteCars/FavoriteCars.tsx
@@ -1,20 +1,44 @@
 import * as React from 'react';
 import AdsContainer from '../AdsContainer/AdsContainer';
+import { IAdStore } from '../../stores/AdStore/AdStore';
+import { inject, observer } from 'mobx-react';
+import LoadingIndicatior from '../LoadingIndicator/LoadingIndicator';
 
 interface FavoriteCarsProps {
-
+    adStore?: IAdStore;
 }
 
 interface FavoriteCarsState {
-
+    page: number;
+    isAdsLoading: boolean;
 }
 
+@inject('adStore')
+@observer
 export default class FavoriteCars extends React.Component<FavoriteCarsProps, FavoriteCarsState> {
+    public state: FavoriteCarsState = {
+        page: 1,
+        isAdsLoading: true
+    }
+
+    public async componentDidMount(): Promise<void> {
+        await this.props.adStore.getFavoriteCars(this.state.page.toString());
+
+        if (this.props.adStore.favoriteCars && this.props.adStore.favoriteCars.allCars) {
+            this.setState({
+                isAdsLoading: false
+            })
+        }
+    }
+
     public render() {
         return (
-            <div>
-                <AdsContainer />
-            </div>
+            <>
+                {this.state.isAdsLoading ?
+                    <LoadingIndicatior /> :
+                    <AdsContainer carsList={this.props.adStore.favoriteCars.allCars}/>}
+
+            </>
         );
     }
 }
