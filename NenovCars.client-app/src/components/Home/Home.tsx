7d93a268cb962aa4a0da066c1f2bda8860986ef8
IvanNenov <nenow@abv.bg>
2019-12-08 17:04:58 +0200

    Added header image in home component

--- NenovCars.client-app/src/components/Home/Home.tsx
+++ NenovCars.client-app/src/components/Home/Home.tsx
@@ -4,10 +4,15 @@ import { inject, observer } from 'mobx-react';
 import { IAdStore } from '../../stores/AdStore/AdStore';
 import LoadingIndicatior from '../LoadingIndicator/LoadingIndicator';
 import { IUiStore } from '../../stores/UiStore/UiStore';
+import { Divider, Pagination, Row, Col, Button } from 'antd';
+import './Home.css';
+import { IUserStore } from '../../stores/UserStore/UserStore';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
 
 interface HomeProps {
     adStore?: IAdStore;
     uiStore?: IUiStore;
+    userStore?: IUserStore;
 }
 
 interface HomeState {
@@ -17,8 +22,9 @@ interface HomeState {
 
 @inject('adStore')
 @inject('uiStore')
+@inject('userStore')
 @observer
-export default class Home extends React.Component<HomeProps, HomeState> {
+class Home extends React.Component<HomeProps & RouteComponentProps, HomeState> {
     public state: HomeState = {
         page: 1,
         isAdsLoading: true
@@ -41,7 +47,65 @@ export default class Home extends React.Component<HomeProps, HomeState> {
 
     public render() {
         return (
-            <>{this.state.isAdsLoading ? <LoadingIndicatior /> : <AdsContainer carsList={this.props.adStore.carsContainer.allCars} />}</>
+            <>
+                {this.state.isAdsLoading ? (
+                    <LoadingIndicatior />
+                ) : (
+                    <>
+                        <div className="dark-translucent">
+                            <h1 className="n-home-title">Find Your Future Car</h1>
+                            <div className="n-separator"></div>
+                            <div className="n-home-buttons">
+                                {this.props.userStore.isLoggedIn ? (
+                                    <>
+                                        <Button ghost onClick={(): void => this.props.history.push('/create-ad')}>Create Ad</Button>
+                                        <Button className="n-home-second-btn" ghost onClick={(): void => this.props.history.push('/search')}>
+                                            Browse
+                                        </Button>
+                                    </>
+                                ) : (
+                                    <>
+                                        <Button ghost onClick={(): void => this.props.history.push('/login')}>Login</Button>
+                                        <Button className="n-home-second-btn" ghost onClick={(): void => this.props.history.push('/register')}>
+                                            Register
+                                        </Button>
+                                    </>
+                                )}
+                            </div>
+                        </div>
+                        <Row type="flex" justify="center">
+                            <Col>
+                                <div className="n-recently-added">
+                                    <Divider>Recently Added</Divider>
+                                </div>
+                                <Pagination
+                                    onChange={this.onPageChange}
+                                    current={this.state.page}
+                                    defaultCurrent={this.state.page}
+                                    total={parseInt(this.props.adStore.carsContainer.totalPagesCount) * 10}
+                                />
+                                <AdsContainer carsList={this.props.adStore.carsContainer.allCars} />
+                                <Pagination
+                                    onChange={this.onPageChange}
+                                    current={this.state.page}
+                                    defaultCurrent={this.state.page}
+                                    total={parseInt(this.props.adStore.carsContainer.totalPagesCount) * 10}
+                                />
+                            </Col>
+                        </Row>
+                    </>
+                )}
+            </>
         );
     }
+
+    private onPageChange = (page: number, pageSize?: number): void => {
+        this.setState({
+            page: page
+        });
+
+        this.props.adStore.getAllCars(page.toString());
+    };
 }
+
+export default withRouter(Home);
