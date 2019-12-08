b55064b14a2bc30d409b1f1aef6c798a53ea74bb
IvanNenov <nenow@abv.bg>
2019-12-08 17:07:27 +0200

    Removed unused service

--- NenovCars.client-app/src/services/Car/CarService.ts
+++ NenovCars.client-app/src/services/Car/CarService.ts
@@ -1,26 +1,7 @@
 import Axios from 'axios';
-import { ICarAdInput } from '../../components/CarAd/interfaces/ICarAdInput';
+import { ICarAdInput } from '../../components/CreateAd/interfaces/ICarAdInput';
 import Auth from '../../helpers/Auth/Auth';
 
 export default class CarService {
-    public async addCar(carAd: ICarAdInput): Promise<boolean> {
-        let config = {
-            headers: {
-                Authorization: `Bearer ${Auth.getAuthToken()}`,
-                UserId: `${Auth.getCurrentUserId()}`
-            }
-        };
-
-        let isSuccessfull: boolean = false;
-
-        try {
-            let result = await Axios.post('api/car/addCar', carAd, config);
-
-            isSuccessfull = result.status === 200 ? true : false;
-        } catch (error) {
-            console.log(error);
-        }
-
-        return isSuccessfull;
-    }
+    
 }
