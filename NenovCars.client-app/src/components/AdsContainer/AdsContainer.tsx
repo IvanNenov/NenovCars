1b6ce655e8bd1fe843db10f35c12ac731b02cf67
IvanNenov <nenow@abv.bg>
2019-12-08 17:02:58 +0200

    Layout change

--- NenovCars.client-app/src/components/AdsContainer/AdsContainer.tsx
+++ NenovCars.client-app/src/components/AdsContainer/AdsContainer.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import { IAdStore } from '../../stores/AdStore/AdStore';
 import Ad from './Add/Ad';
-import { List, Row, Col } from 'antd';
+import { List } from 'antd';
 import { inject, observer } from 'mobx-react';
 import { ICarAd } from './interfaces/ICarAd';
-import './AdsContainer.css';
 import { IUiStore } from '../../stores/UiStore/UiStore';
+import './AdsContainer.css';
 
 interface AdsContainerProps {
     adStore?: IAdStore;
@@ -13,36 +13,24 @@ interface AdsContainerProps {
     carsList?: ICarAd[];
 }
 
-interface AdsContainerState {
-    page: number;
-    isAdsLoading: boolean;
-}
+interface AdsContainerState {}
 
 @inject('adStore')
 @inject('uiStore')
 @observer
 export default class AdsContainer extends React.Component<AdsContainerProps, AdsContainerState> {
-    public state: AdsContainerState = {
-        page: 1,
-        isAdsLoading: true
-    };
-
     public render(): JSX.Element {
         return (
             <div>
-                <Row type="flex" justify="center">
-                    <Col>
-                        <List
-                            itemLayout="horizontal"
-                            dataSource={this.props.carsList && this.props.carsList}
-                            renderItem={item => (
-                                <List.Item>
-                                    <Ad ad={item} />
-                                </List.Item>
-                            )}
-                        />
-                    </Col>
-                </Row>
+                <List
+                    itemLayout="horizontal"
+                    dataSource={this.props.carsList && this.props.carsList}
+                    renderItem={item => (
+                        <List.Item>
+                            <Ad ad={item} />
+                        </List.Item>
+                    )}
+                />
             </div>
         );
     }
